8b2feea1ed9bf2ce4174b782f0f22afd331bf5a8
Kolapo <kolapsganiyu@gmail.com>
2024-12-03 20:17:49 +0100

    new changes made

--- FileCraft.py
+++ FileCraft.py
@@ -1,21 +1,44 @@
+import os
+import time
 # creating a file that creates and deletes files
 
 files = []
+
+wD= os.getcwd() # getting the current working directory
+def warn():
+    print("!! remember to include the extensions")
+    return None  
+def loading():
+    bootSymbol = '-'
+    while True:
+        bootSymbol += '-'
+
 
 while True: 
     print("Create[c] or delete[d] files")
     userInput = input("-- ")
     if userInput == "c":
-        print("!! remember to include the extensions")
-        fileName = input("what is the name of the file you want to create -- ")
+        # asking to write into the new file
         try: 
-            newFile = open(fileName , 'x')
+            warn()
+            fileName = input("name of the file --")
+            newFile = open(os.path.join(wD , "x"))
+            print("this worked")
             writeCond = input("do you want to write into the newly created file[y/n]")
             if writeCond == "y":
                 newText = input("--")
                 newFile.write(newText)
+            else:
+                exit()
             newFile.close()
         except:
             print("File already Exists")
-        
+    elif userInput == "d":
+        warn()
+        print("What file do you want to delete in this directory")
+        fileName = input("--")
+        time.sleep()
+        os.remove(fileName)
+
+
 
